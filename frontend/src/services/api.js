2bf75e6adf440def4b9788d744e7ff1024b805d2
parth-sanrusha <Parth.Gopani@sanrusha.com>
2026-03-22 19:58:31 +0530

    Updates

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  // baseURL: "http://localhost:5000",
-    baseURL: "https://embroidex_backend.merishiksha.com",
-
-});
-
-export default API;
