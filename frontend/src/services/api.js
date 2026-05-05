66e0eb3e0302ad0225d677db6467d1ee2fd46049
parth-sanrusha <Parth.Gopani@sanrusha.com>
2026-05-05 14:56:56 +0530

    google-genai package added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
-    // baseURL: "https://embroidex-backend.merishiksha.com",
+  // baseURL: "http://localhost:5000",
+    baseURL: "https://embroidex-backend.merishiksha.com",
 
 });
 
