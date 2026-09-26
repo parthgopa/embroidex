c2d186df656b189c55ae006b04650d02e9d0e71f
Parth Gopani <Parth.Gopani@sanrusha.com>
2026-09-26 10:52:31 +0530

    fix(mobile): dynamically apply system bar and display cutout insets on Razorpay CheckoutActivity

--- mobile_app/android/app/src/main/java/com/embroidex/MainApplication.kt
+++ mobile_app/android/app/src/main/java/com/embroidex/MainApplication.kt
@@ -1,6 +1,11 @@
 package com.embroidex
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -22,5 +27,60 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
+
+    registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+      override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        applyRazorpayInsets(activity)
+      }
+
+      override fun onActivityStarted(activity: Activity) {
+        applyRazorpayInsets(activity)
+      }
+
+      override fun onActivityResumed(activity: Activity) {
+        applyRazorpayInsets(activity)
+      }
+
+      override fun onActivityPaused(activity: Activity) {}
+      override fun onActivityStopped(activity: Activity) {}
+      override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+      override fun onActivityDestroyed(activity: Activity) {}
+    })
+  }
+
+  private fun applyRazorpayInsets(activity: Activity) {
+    val className = activity.componentName?.className ?: activity.javaClass.name
+    if (!className.contains("CheckoutActivity", ignoreCase = true) && !className.contains("razorpay", ignoreCase = true)) {
+      return
+    }
+
+    val window = activity.window ?: return
+    val decorView = window.decorView ?: return
+
+    val applyPadding = {
+      val content = activity.findViewById<View>(android.R.id.content)
+      val insets = ViewCompat.getRootWindowInsets(decorView)
+      if (content != null && insets != null) {
+        val bars = insets.getInsets(
+          WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+        )
+        if (bars.top > 0 || bars.bottom > 0) {
+          content.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+        }
+      }
+    }
+
+    ViewCompat.setOnApplyWindowInsetsListener(decorView) { _, insets ->
+      val bars = insets.getInsets(
+        WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+      )
+      val content = activity.findViewById<View>(android.R.id.content)
+      content?.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+      insets
+    }
+
+    decorView.post {
+      applyPadding()
+    }
   }
 }
